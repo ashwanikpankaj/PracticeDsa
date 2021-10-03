3ae9dc96fe4dea2ce798e75f7ae74028bbbfa116
ashwanikpankaj <ashnit8294@gmail.com>
2021-10-03 19:36:48 +0530

    reverse

---
+++ practice_dsa unit-3/linkedlist/reverseLinkedList.js
@@ -0,0 +1,28 @@
+
+const LinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+}
+
+// Complete the function below
+
+function reverse(head) {
+ 
+ if(head == null){
+     return null;
+ }
+ let curr = head;
+ let prev = null;
+ let next = null;
+
+while(curr!=null){
+  next = curr.next;
+  curr.next = prev;
+  prev = curr;
+  curr = next;
+}
+ head = prev;
+ return head
+}
